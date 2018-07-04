2dcef48c7c94ef61cd654850ee95a5bea37ced50
장건우 <zmdlgl@gmail.com>
2018-07-04 22:28:30 +0900

    complate

---  test/sum.spec.js
+++  test/sum.spec.js
@@ -1,10 +1,10 @@
-const {sum1} = require('../src/sum');
+const { twoCalculation } = require('../src/sum');
 
-describe('sum', ()=>{
-    describe('sum1', ()=>{
-        it('get two parameters and return sum result', ()=>{
-            expect(sum1(1,2)).toBe(3);
-            expect(sum1(3,4)).toBe(7);
+describe('sum', () => {
+    describe('sum1', () => {
+        it('get two parameters and return sum result', () => {
+            expect(twoCalculation(13)).toBe(2);
+            expect(twoCalculation(5)).toBe(1);
         })
     })
 })
--- src/sum.js
+++ src/sum.js
@@ -1,4 +1,10 @@
-console.log('dd')
-exports.sum1 = function(num1,num2){
-    return num1+num2;
+function twoCalculation(num) {
+    const transNumStr = num.toString(2)
+    //const countOne = transNumStr.match(/1/g)
+    const oneArray = transNumStr.split('0')
+    return Math.max(...oneArray).toString().length
+}
+
+module.exports = {
+    twoCalculation
 }
